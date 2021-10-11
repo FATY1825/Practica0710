923f09879f47dc7d0e26ca3a12d9fc403493a0a7
Fatima Ayala <ftayala89@gmail.com>
2021-10-10 22:15:35 -0600

    Actualizacion

--- Practica0710/frmCliente.cs
+++ Practica0710/frmCliente.cs
@@ -16,5 +16,10 @@ namespace Practica0710
         {
             InitializeComponent();
         }
+
+        private void metroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
